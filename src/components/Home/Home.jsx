377fa5ee9d94f07420bd159423076f36805f6ed1
muna <muna3828@gmail.com>
2023-09-15 19:17:29 +0600

    ctrate all cards dynamically for all courses

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,46 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Home.css"
+import { FaDollarSign, FaBookOpen } from "react-icons/fa";
 
 
 const Home = () => {
+
+    const [allCourses, setAllCourses]= useState([])
+
+useEffect(()=>{
+    fetch("./card.json")
+    .then(res=>res.json())
+    .then((data)=> setAllCourses(data))
+},[])
+
+console.log(allCourses)
+
     return (
-        <div className='card-container'>
+        <div className="container">
+            <div className='card-container'>
             <h2>Course Registration</h2>
-            <div className="card">
-                <img className='photo' src="https://i.ibb.co/ZJv51cg/hitesh-choudhary-D9-Zow2-REm8-U-unsplash.jpg" alt="" />
-                <h3>Introduction to Python</h3>
-                <p>Learn the fundamentals of Python programming, including syntax, data types, and control structures</p>
+            <div className="allCards">
+            {
+                allCourses.map(card =>(
+                    <div key={card.id} className="card">
+                <img className='photo' src={card.image_url} alt="" />
+                <h3>{card.course_name}</h3>
+                <p><small>{card.course_details}</small></p>
                 <div className="price-credit">
-                    <p>price: 300</p>
-                    <p>credit: 2 hr</p>
+                    <p><FaDollarSign></FaDollarSign>  price: {card.price}</p>
+                    <p><FaBookOpen></FaBookOpen>   credit: {card.course_credit} hr</p>
                 </div>
                 <button className='btn'>Select</button>
             </div>
+                ))
+            }
+            </div>
+        </div>
+        <div className="cart">
+            <h4>Credit Hour Remaining 7 hr</h4>
+        </div>
         </div>
     );
 };
